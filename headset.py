2bc70599c5066fa0a5e4c6087eba03c829d51e42
Emil <nudelkurre@protonmail.com>
2022-03-07 13:31:06 +0100

    Removed output when headset is off

--- headset.py
+++ headset.py
@@ -4,9 +4,7 @@ import subprocess
 
 proc = subprocess.Popen(["headsetcontrol","-b", "-c"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 bat_level = proc.communicate()[0]
-if(bat_level == b'0'):
-	print("Off")
-elif(bat_level == b''):
+if(bat_level == b'0' or bat_level == b''):
 	print("")
 else:
 	print(str(int(bat_level)) + "%")
